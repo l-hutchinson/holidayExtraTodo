3e9dcc1a38221cd42a015ba5b50d871fa72a0942
Lisa Hutchinson <lisa@Lisas-MacBook-Pro.local>
2022-08-18 10:20:27 +0100

    TODO list push

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,79 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
 
-function App() {
+type TodoListType = {
+  id: number;
+  todoItem: string;
+  complete: boolean;
+};
+
+const App = () => {
+  const [todoTxt, setTodoTxt] = useState<string>('');
+  const [todoList, setTodoList] = useState<TodoListType[]>([]);
+
+  const handleTxtEntry = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setTodoTxt(e.target.value);
+  };
+
+  const handleAddTodo = () => {
+    setTodoList([
+      ...todoList,
+      {
+        id: todoList.length + 1,
+        todoItem: todoTxt,
+        complete: false,
+      },
+    ]);
+    setTodoTxt('');
+  };
+
+  const handleCompletion = (id: number) => {
+    setTodoList(
+      todoList.map((i: TodoListType) =>
+        i.id === id ? { ...i, complete: !i.complete } : i
+      )
+    );
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div
+      style={{
+        display: 'flex',
+        justifyContent: 'center',
+        margin: '5% 0',
+      }}
+    >
+      <div style={{ width: 'fit-content' }}>
+        <h4>TODO App Holiday extras</h4>
+        <input
+          type="string"
+          placeholder="add task"
+          value={todoTxt}
+          onChange={handleTxtEntry}
+        />
+        <button onClick={handleAddTodo}>Add</button>
+
+        <br />
+
+        <div>
+          {`${todoList.filter((i) => i.complete).length} remaining out of ${
+            todoList.length
+          } tasks`}
+        </div>
+
+        <ul>
+          {todoList.map((i) => (
+            <li
+              key={i.id}
+              style={{ textDecoration: i.complete ? 'line-through' : 'none' }}
+              onClick={() => handleCompletion(i.id)}
+            >
+              {i.todoItem}
+            </li>
+          ))}
+        </ul>
+      </div>
     </div>
   );
-}
+};
 
 export default App;
